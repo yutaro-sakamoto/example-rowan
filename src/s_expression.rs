d768464107265d7971814326e2f96d0d5f28e47a
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-01-24 16:00:53 +0900

    wip: wip

--- src/s_expression.rs
+++ src/s_expression.rs
@@ -303,6 +303,20 @@ impl Parse {
     }
 }
 
+fn main() {
+    let sexps = "
+92
+(+ 62 30)
+(/ 92 0)
+nan
+(+ (* 15 2) 62)
+";
+    let root = parse(sexps).root();
+    let res = root.sexps().map(|it| it.eval()).collect::<Vec<_>>();
+    eprintln!("{:?}", res);
+    assert_eq!(res, vec![Some(92), Some(92), None, None, Some(92),])
+}
+
 fn lex(text: &str) -> Vec<(SyntaxKind, String)> {
     fn tok(t: SyntaxKind) -> m_lexer::TokenKind {
         m_lexer::TokenKind(rowan::SyntaxKind::from(t).0)
